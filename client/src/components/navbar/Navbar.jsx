ee67e1dd60336cfc194accc6a8fb97ec7babc18d
Kris <kharonek@yahoo.com>
2023-08-03 22:57:06 -0300

    feat: add logout functionality

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { useSelector } from "react-redux";
+import { useLogout } from "../../hooks/useLogout";
+
 import { FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
+  const auth = useSelector((state) => state.auth);
+
+  const { logout } = useLogout();
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
     <div className="text-white bg-green-600 w-full h-[60px]">
       <div className="max-w-7xl mx-auto px-4 flex justify-between items-center h-full">
@@ -14,14 +24,27 @@ const Navbar = () => {
         {/* Login */}
         <div className="hidden sm:flex h-full">
           <ul className="flex items-center">
-            <Link to="/login" className="h-full flex items-center cursor-pointer hover:bg-green-700">
-              <li>Login</li>
-            </Link>
+            {!auth.user ? (
+              <>
+                <Link
+                  to="/login"
+                  className="h-full flex items-center cursor-pointer hover:bg-green-700"
+                >
+                  <li>Login</li>
+                </Link>
 
-            <Link to="/signup" className="h-full flex items-center cursor-pointer hover:bg-green-700">
-              <li>Signup</li>
-            </Link>
-            
+                <Link
+                  to="/signup"
+                  className="h-full flex items-center cursor-pointer hover:bg-green-700"
+                >
+                  <li>SignUp</li>
+                </Link>
+              </>
+            ) : (
+              <li onClick={handleLogout} className="h-full flex items-center cursor-pointer hover:bg-green-700">
+                Logout
+              </li>
+            )}
           </ul>
         </div>
       </div>
